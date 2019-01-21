c7f3823875547dadff28af7d4b5d4939f2b501cf
Steven Snow <s40w5s@gmail.com>
2019-01-21 07:43:46 -0500

    Added this silverblue-logo.sh ascii art

---
+++ public/silverblue-logo.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+clear
+cat << X0
+[38;5;153m              __assssssssssssssssssssssssssssssss[38;5;123miiiiiiiiiii[38;5;4m,
+           [38;5;117m.%nSSonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnonooonon[38;5;4ml;
+[38;5;12m          <oooonoooooooooooooooooooooooooooooooooooooooooo[38;5;4ml|;
+         [38;5;12mvoooooooo2ooo2ooooo2ooooo2ooooo2ooooo2ooo2oo2o2o[38;5;4mli|;
+        [38;5;12muooo2o2o2ooo2ooo2o2ooo2o2ooo2o2ooo2o2ooo2ooo2ooo[38;5;4ml||i;
+       [38;5;12mvnoo2oooooo2oo2o2oooo2oooo2o2oooo2oooo2o2oo2oooo[38;5;4ml|i|i;
+      [38;5;12m-oooooo2o2o2oo2ooooo2oo2o2oooooo2oo2o2oooooooo2o[38;5;4mli|i||;
+      [38;5;12mooo2o2ooooooo2ooo2o2oo2oooo2o2o2oo2oooo2o2o2o2o[38;5;4ml||i||i;
+     [38;5;12m-2ooooo2o2ooXoooo2oooo2ooo2Xoooooo2ooo2ooooXnoo[38;5;4ml|i||i|i;
+     [38;5;12mvoo2o2oooom[38;5;253mQQQ[38;5;12mmnooo2o2oo2m[38;5;253mQQQ[38;5;12mmo2o2oo2oooq[38;5;253mQQQ[38;5;12mmp[38;5;4mi|i||i|i|;
+     [38;5;12m2ooooo2o2[38;5;253mQQQQQQ[38;5;12mpoo2ooooom[38;5;253mWQQQQ[38;5;12mmooooooo2q[38;5;253mQQQQQQ[38;5;4m>||i|i|||;
+    [38;5;12m:oo2o2ooo[38;5;253mXQQ@VQQQn[38;5;12mooo2o22[38;5;253mQQWVWWQX[38;5;12moo2o2oo[38;5;253mmQQVSQQe[38;5;4mii||i|i|;
+    [38;5;12m-ooooo2o2[38;5;253mdQQ[38;5;12mnod[38;5;253mQQ[38;5;12moo2oooo[38;5;253mXQQ[38;5;12mknd[38;5;253mWQE[38;5;12mnooooo2[38;5;253mQQ#[38;5;12mn[38;5;4mi[38;5;253mQWk[38;5;4mi||i|||i;
+    [38;5;12m-o2o2oooo[38;5;253mdQQ[38;5;12mmo[38;5;253mQQQ[38;5;12moooo2o2[38;5;253mXWQ[38;5;12mmXm[38;5;253mQQk[38;5;12mnoo2o2o[38;5;253mQWQ[38;5;4mza[38;5;253mQQk[38;5;4m||i||ii|;
+    [38;5;12m-oooo2o2o[38;5;253mSQQQQQQD[38;5;12mno2oooon[38;5;253mSQQQWQW[38;5;12mnnoooooo[38;5;253mmQQQQQQ[38;5;4m(i||i|i||;
+    [38;5;12m-2o2oooooo[38;5;253mdWQQQB[38;5;12mnoooo2o2oX[38;5;253mWWQQWC[38;5;12mooo2o2o[38;5;253mQQQQQQWC[38;5;4m||i|i|||i;
+    [38;5;12m-oooo2o2ooo[38;5;253moQQ#[38;5;12mooo2o2oooonn[38;5;253mSQWo[38;5;12mooo2ooo[38;5;253mQWQD*[38;5;4mYY|||i|||i|i|;
+    [38;5;12m-o2o2oooo2o[38;5;253moWQ#[38;5;12mooooooo2o2oo[38;5;253mmQWo[38;5;12moo2ooo[38;5;253moQWQD[38;5;4m|||||i||ii||i|;
+    [38;5;12m-2oooo2o2oo[38;5;253moQQ#[38;5;12moo2o2o2ooooo[38;5;253mmQWo[38;5;12mo2ooo[38;5;253mQQQD[38;5;4m||i||i|i|i|||i|i;
+    [38;5;12m-oo2o2oooo2[38;5;253moWQ#[38;5;12moooooooo2o2o[38;5;253mmQWo[38;5;12moooo[38;5;253mQWQD[38;5;4m||i|i|i||i||i|i||;
+    [38;5;12m-o2oooo2o2o[38;5;253moQQ#[38;5;12mo2o2o2o2oooo[38;5;253mmQQo[38;5;12mo2o[38;5;253mQQQD[38;5;4m||i||i||i||i|i||i|;
+    [38;5;12m-oooo2ooooo[38;5;253moQQ#[38;5;12mooooooooo2o2[38;5;253mmQWn[38;5;12moo[38;5;253mQWQD[38;5;4m||i||i||i|i||i||i|i;
+    [38;5;12m-o2o2oo2o2o[38;5;253moQW#[38;5;12moo2o2o2o2ooo[38;5;253mmQWo[38;5;12mo[38;5;253mQWQD[38;5;4m||i||i|i|i||i|i|i||i;
+    [38;5;12m-ooooo2oooo[38;5;253m2WQ#[38;5;12mo2oooooooo2o[38;5;253mmQWoQWQD[38;5;4m||i||i||i||i||i|||i|i;
+    [38;5;12m-o2o2ooo2o2[38;5;253moWQ#[38;5;12mooo2o2o2o2oo[38;5;253mmQQQWQD[38;5;4m||i||i|i[38;5;253myQQw%[38;5;4m|i||i|i||;
+    [38;5;12m-ooooo2oooo[38;5;253moQQ#[38;5;12mo2oooooooooo[38;5;253mmQQWQD[38;5;4m||i||i|i[38;5;253mQQWWQm%[38;5;4m|i|i||i|;
+    [38;5;12m-o2o2ooo2o2[38;5;253moWQ#[38;5;12mooo2o2o2o2o2[38;5;253mmQQQQwwwwwwww[38;5;253myQQDHWQk[38;5;4m||i||i|i;
+    [38;5;12m-ooooo2oooo[38;5;253moQQ#[38;5;12mo2oooooo2ooo[38;5;253mQQQQQWQWQQWQQQQB[38;5;4m||[38;5;253mWQm[38;5;4m|i||i|i|;
+    [38;5;12m-oo2o2oo2o2[38;5;253moWQ#[38;5;12mooo2o2o2ooo[38;5;253mQQWQWQWQWQWQWQQQQ[38;5;4m%i[38;5;253mQWE[38;5;4m|||i|i||;
+    [38;5;12m-o2oooo2ooo[38;5;253moQQ#[38;5;12mo2oooo2ooo[38;5;253mQQQD[38;5;4m|||||||||||[38;5;253m3WQQWWQC[38;5;4m|i|i|||i;
+    [38;5;12m-ooo2o2oo2o[38;5;253m2QQ#[38;5;12mooo2o2ooo[38;5;253mQQQD[38;5;4m|||i|i|i|i|i|[38;5;253m9QQWQE[38;5;4m||||i|i|i;
+    [38;5;12m-oo2ooooooo[38;5;253moQW#[38;5;12mo2oooooo[38;5;253mQQQE[38;5;4m|||i||i||i|i||i[38;5;253m?TT1[38;5;4m|||i||i||i;
+    [38;5;12m-oooo2o2o2o[38;5;253m2QW#[38;5;12mooo2o2o[38;5;253mQQQE[38;5;4m|||i||i||i|||i|i|i|i||i||i||i|;
+    [38;5;12m-oo2ooooooo[38;5;253moWQ#[38;5;12m2o2ooo[38;5;253mQQQE[38;5;4m||i|i|i|i|i|i|i||i|i||i||i|i|i|;
+    [38;5;12m-oo2o2o2o2o[38;5;253m2QQm[38;5;12mooooo[38;5;253mQQQE[38;5;4m||i|||i|||i||i||i||i||i||i|i|||i;
+    [38;5;12m-oooooooooo[38;5;253moQWm[38;5;12moo2o[38;5;253mQQQE[38;5;4m||i||i|i|i|i|i||i|i|i|i|i|i|||ii|;
+    [38;5;12m-oo2o2o2o2o[38;5;253moWQm[38;5;12mooo[38;5;253mQQQE[38;5;4m||i||i|i|||i|||i|i||i|||i||i|i|i||;
+    [38;5;12m-ooooooooo2[38;5;253moQQ#[38;5;12moo[38;5;253mQQQE[38;5;4m||i||i||i|i|i|i|i||i||i|i||i|||i||i;
+    [38;5;12m-oo2o2o2o2o[38;5;253moQQ#[38;5;12mo[38;5;253mQQQE[38;5;4m||i||i|i||i||i||i||i|i|i||i|i|i|i|i|;
+    [38;5;12m-oooooooooo[38;5;253moWQmQQQE[38;5;4m||||i||i||i||i||i||i||[38;5;253miwmmw[38;5;4m||i||i|||i;
+    [38;5;12m-oo2o2o2o2o[38;5;253m2WQQQQE[38;5;4m|||i|i|i||i|i|i|i||i|ii[38;5;253mmQQQQm[38;5;4mi||i||i|i;
+    [38;5;12m-ooooooo2oo[38;5;253moQQQQB[38;5;4maaaaaaaaaaaaaaaaaaaaaaaq[38;5;253mQQ@SQQz[38;5;4mi||i|i||;
+    [38;5;12m-ooo2o2ooo2[38;5;253moWQQQQWQWQQQQWQQQQQWQQWQQQQQWQWW[38;5;4m||[38;5;253mQWm[38;5;4m|i|i||i|;
+    [38;5;12m:oo2oooo2[38;5;253moooQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ[38;5;4m||[38;5;253mQWD[38;5;4m|i||i||i;
+    [38;5;12m-oooo2o2[38;5;253moo2nY?Y?Y?Y?Y?Y?Y?Y?Y?Y?Y?Y?Y?Y?9QWQmQQC[38;5;4m||i|i|i|:
+    [38;5;12m:o2o2oo[38;5;253mooon[38;5;4m||||||i|||i|||||||||||i|||||ii[38;5;253mSQWQQD[38;5;4m|i|i|||i|:
+    [38;5;12m-ooooo[38;5;253m2o2n[38;5;4miii|ii||i|i||i|ii|ii|i|i|i|i||||[38;5;253mYHHY[38;5;4m|||i||i|i|
+    [38;5;12m:o2o2[38;5;253mooon[38;5;4mi|||i||i||i||i|i||i||i|i|||i|i|i|||||||i||i|i|;
+    [38;5;12m:2oo[38;5;253moo2n[38;5;4mi|i|i||i|i|i|i||i|i||i|||i|i||i|i|i||||i||i||i|
+    [38;5;12m:2o[38;5;253mo2on[38;5;4mi|i|i|i|i||i|||i|i|||i|i|i|i|i||i||i|i|i||i|i||;
+    [38;5;12m:2[38;5;253mooon[38;5;4mi|i|||i||i|i||i|i||i|i||i||i|||i||i||i|i|i|i||i>
+    [38;5;253m:oo2n[38;5;4mi|i||i|i|i|||i||i||i||i|i||i||i|i|i|i||i|||i||i|
+    [38;5;253m:2on[38;5;4m|i|i|i|i|||i|i|i|i|i|i|i||i|i|i|i||i||i|i|i||i|>
+    [38;5;253m:on[38;5;4mii|i|||i||i|i||i|||i|||i||i||i||i||i||i||i||i|i+
+    [38;5;253m:n[38;5;4mi||i||i|i|i|i||i||i|i|i|i|i||i||i||i|i||i|i|i|+,
+    -++++++++++++++++++|++++++++++|+++++|++++|+++~,
+    ___ _ _             _    _
+   / __(_) |_ _____ _ _| |__| |_  _ ___
+   \__ \ | \ V / -_) '_| '_ \ | || / -_)
+   |___/_|_|\_/\___|_| |_.__/_|\_,_\___|
+[0m
+X0
