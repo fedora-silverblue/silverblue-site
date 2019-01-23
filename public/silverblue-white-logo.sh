7b040e11640591c114ad145e0b8ed9aa45b90c57
Steven Snow <s40w5s@gmail.com>
2019-01-23 10:37:00 -0500

    Added this silverblue-logo.sh ascii art (#57)
    * Added this silverblue-logo.sh ascii art
    
    * Add another Silverblue Logo ascii art shell script
    
    * Silverblue-white-logo.sh ascii art

---
+++ public/silverblue-white-logo.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/zsh
+
+cat << X0
+[0m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                    [0;10m[1m_[0;10mgg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1ma[0;10mg[1m,[0;10m[8m            [0;10m
+[8m                  [0;10mq[1myQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m                 [0;10m[1mjQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m               .[0;10m[1mjQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m               [0;10mj[1mQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              =[0;10m[1mmQQQQQQWQQQQQQQQWQQQQQQQQQWQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10mj[1mQQQQQD^[0;10m[8m [0;10m7[1mQWQQQQ![0;10m[8m [0;10m-[1m4WQQQQ@^[0;10m[8m [0;10m][1mSQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQ[0;10m [1m.a[0;10m[8m [0;10m[1m]WQQQE[0;10m[8m [0;10mqp[8m [0;10m[1mSQQQQ[0;10m _[1ma[0;10m [1m)WQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQ[0;10m [1m)W[0;10m j[1mQQQQL[0;10m[8m [0;10m[1m4f[0;10m[8m [0;10m[1mdQQQQ[0;10m ][1mW[0;10m ][1mQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1mjQQQQQc[0;10m[8m  .[0;10m[1mjQQQQm[0;10m,[8m X[0;10m[1m_QQQQ@ [0;10m[8m v [0;10m[1m,@fjQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQc[0;10m[8m [0;10m[1mjQQQQQQm[0;10m [1m_mQQQ@^[0;10m[8m:[0;10m[1ma[0;10mg[1mwWQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1mjQQQQQQk[0;10m[8m [0;10m[1mmQQQQQQQ[0;10m j[1mQQQ@^[0;10m[8m=[0;10m[1mjQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mdQQQQQQQ[0;10m j[1mQQW^[0;10m[8m+[0;10m[1mjQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1mjQQQQQQk[0;10m[8m [0;10m[1mmQQQQQQQ[0;10m j[1mQW^[0;10m[8m [0;10m[1mjQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mdQQQQQQQ[0;10m j[1mW^[0;10m[8m [0;10m[1mjQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1mjQQQQQQk[0;10m[8m [0;10m[1mmQQQQQQQ[0;10m [1m^[0;10m[8m [0;10m [1mjQQQQP?4QQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mdQQQQQQQ[0;10m [8m =[0;10m[1mjWWWWf[0;10m[8m   [0;10m[1m]QQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mmQQQQQQ@^[0;10m[8m        [0;10m][1mQ[0;10m )[1mQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1mjQQQQQQk[0;10m[8m [0;10m[1mdQQQQQ@^[0;10m[8m [0;10m[1maaaaaaa,[0;10m-[1m?[0;10m j[1mQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mmQQQQ@^[0;10m[8m [0;10m[1mjQWQWQWQm,[0;10m[8m [0;10m[1m_mQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mdQQQ@^[0;10m[8m [0;10m[1mjQQQQQQQQQQmQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mmQQ@^[0;10m[8m [0;10m[1mjQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mdQ@^[0;10m[8m [0;10m[1mjQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1mm@^[0;10m[8m [0;10m[1mjQQQQQQQQQQQQQQQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m[1md^[0;10m[8m [0;10m[1mjQQQQQQQQQQQQQQBQQQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m [0;10m^[8m [0;10m[1mjQQQQQQQQQQQQQ@^[0;10m[8m [0;10m)[1mSWQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m  [0;10m][1m??????????????^[0;10m_[1ma[0;10m ][1mQQQQQ([0;10m[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQk[0;10m[8m                  [0;10m][1mW[0;10m ][1mQQQQQ[0;10mL[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQWQmL[0;10m[8m   [0;10m[1mjWQQQQ[0;10m[[8m            [0;10m
+[8m              [0;10m[1m]QQQQQQQQWWQWWWWWWWWWWWWQQwaaQWQQQ@[0;10m[8m             [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQQQQQQWQQQQQQ[[0;10m[8m             [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ@[0;10m^[8m             [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQW^[0;10m[8m              [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ@^[0;10m[8m               [0;10m
+[8m              [0;10m[1m]QQQQQQQQQQQQQQQQQQQQQQQQQQQWD^[0;10m[8m                 [0;10m
+[8m              [0;10m-[1m~[0;10m^[1m~[0;10m^[1m~[0;10m^[1m~[0;10m^^^^^^^^^^^^^^^^^^[8m                   [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m              =[0;10m[1m_, [0;10m[8m [0;10m[1m.[0;10m[8m [0;10m,[8m [0;10m _,[8m [0;10m[1m.[0;10m[8m [0;10m[1m _[0;10mw, _[1m_, [0;10m[8mX[0;10m_ [1m_,[0;10m[8mn[0;10m_,[8m  [0;10m [1m.[0;10m[8m  [0;10m,  _[1m__[0;10m[8m             [0;10m
+[8m              [0;10m[1m]~* [0;10m[8m=[0;10m[1m{[0;10m[8m [0;10m[1m[[0;10m[8m [0;10m )[1m[[0;10m[8m [0;10m[1m%[0;10m) [1mf^^ ]^^[ [0;10mj[1m}^[0;10mp jf[8m  [0;10m [1ml [0;10m[8m [0;10mjf[1m ]^^[0;10m[8m=            [0;10m
+[8m              [0;10m][1ms, [0;10m[8m [0;10m[1mi[0;10m[8m.[0;10m[1m[[0;10m[8m  [0;10m[1m i[0;10m_[1m[[0;10m[8mQ[0;10m [1mL,[0;10m[8m [0;10m [1m),[0;10mq[1m( [0;10mj[1mc[0;10mg[1m' [0;10mjf[8m  [0;10m [1ml [0;10m[8m [0;10m]f [1m][0;10mg/[8m             [0;10m
+[8m               [0;10m-[1m{[0;10m, [1mv[0;10m[8m [0;10m[1m[[0;10m[8m  [0;10m[1m ][0;10mj[1m^[0;10m[8mQ[0;10m [1mr[0;10m^[8m [0;10m [1m)^1[0;10m[8m [0;10m jF![1m[ [0;10mjf[8m  [0;10m [1ml [0;10m[8m [0;10mjf [1m][0;10m"'[8m             [0;10m
+[8m              [0;10mj[1m,j[0;10m^ [1mi[0;10m[8m [0;10m[1mL[0;10mw[1m,[0;10m -[1m2[0;10m[8m[0;10m)  [1mc_, )[0;10m^[1m), [0;10mjgg[1mr [0;10mj[1mc_[0;10m  [1m], j[0;10m^ [1m][0;10mww,[8m            [0;10m
+[8m              |[0;10m[1m^ [0;10m[8m [0;10m ^[8m [0;10m![1m~^[0;10m[8m [0;10m ^[8m  [0;10m [1m~~^[0;10m -[8m- [0;10m^- [1m^~[0;10m[8m  [0;10m [1m~~^[0;10m[8m [0;10m [1m^^[0;10m[8m~[0;10m  ^[1m~~[0;10m[8m             [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[8m                                                              [0;10m
+[0m
+X0
