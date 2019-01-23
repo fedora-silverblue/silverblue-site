7d25606cc05f7cdf01b2d1f954b697a37fd261f5
Steven Snow <s40w5s@gmail.com>
2019-01-22 21:57:41 -0500

    Add another Silverblue Logo ascii art shell script

---
+++ public/silverblue_logo_aa.zsh
@@ -0,0 +1,39 @@
+#!/usr/bin/env zsh
+
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What You Want
+# To Public License, Version 0.01, as published by Steven Snow. See
+# this document for more details.
+clear
+cat << X0
+     [0m[38;5;12m(QXXXXXXXXXXXXXXXX/[38;5;24m7[0m
+  [0m[38;5;12m(XXXXXXXXXXXXXXXXXXX/[38;5;24mOO[0m
+ [38;5;12m(XXXXXXXXXXXXXXXXXXX/[38;5;24mOOO[0m
+[38;5;12m(XXXXXXXXXXXXXXXX[38;5;255m(QQo[38;5;24mOOOO[0m
+[38;5;12mPXXXXXXXXXXXXXXX[38;5;255mX[38;5;12mXX/[38;5;255mO)[38;5;24mOOO[0m
+[38;5;12mPXXXXXXXXXXXXXXX[38;5;255mXX/O)[38;5;24mOOOO[0m
+[38;5;12mPXXXXXX[38;5;255mXX[38;5;12mXXXXXX[38;5;255mXX/O[38;5;24mOOOOOO[0m
+[38;5;12mPXXXXX[38;5;255mX[38;5;12mXX[38;5;255mX[38;5;12mXXXX[38;5;255mXX/O[38;5;24mOOOOOOO[0m
+[38;5;12mPXXXXX[38;5;255mX[38;5;12mXX[38;5;255mX[38;5;12mXXX[38;5;255mXX/O[38;5;24mOOOOOOOO[0m
+[38;5;12mPXXXXXX[38;5;255mXX[38;5;12mXXX[38;5;255mXX/O[38;5;24mOOOOOOOOO[0m
+[38;5;12mPXXXXXX[38;5;255mXX[38;5;12mXX[38;5;255mXX/O[38;5;24mOOOOOOOOOO[0m
+[38;5;12mPX[38;5;255mXX[38;5;12mXXX[38;5;255mXX[38;5;12mX[38;5;255mXX/O[38;5;24mOOO[38;5;255mOO[38;5;24mOOOOOO[0m
+[38;5;12mP[38;5;255mX[38;5;12mXX[38;5;255mX[38;5;12mXX[38;5;255mXXXX/OOOOO[38;5;24mOO[38;5;255mO[38;5;24mOOOOO[0m
+[38;5;12mP[38;5;255mX[38;5;12mXX[38;5;255mX[38;5;12mXX[38;5;255mXXX/OOOOOO[38;5;24mOO[38;5;255mO[38;5;24mOOOOO[0m
+[38;5;12mPX[38;5;255mXX[38;5;12mXXX[38;5;255mXX/O[38;5;24mOOOOOO[38;5;255mOO[38;5;24mOOOOOO[0m
+[38;5;12mPX[38;5;255mXX[38;5;12mXX[38;5;255mXX/O[38;5;24mOOOOOOOOOOOOOOO[0m
+[38;5;12mPX[38;5;255mXX[38;5;12mX[38;5;255mXX/O[38;5;24mOOOOOOOOOOOOOOOO[0m
+[38;5;12mPX[38;5;255mXXXX/O[38;5;24mOOOOOOOOO[38;5;255mOO[38;5;24mOOOOOO[0m
+[38;5;12mPX[38;5;255mXXX/OOOOOOOOOOO[38;5;24mOO[38;5;255mO[38;5;24mOOOO)[0m
+[38;5;12mPX[38;5;255mXX/OOOOOOOOOOOO[38;5;24mOO[38;5;255mO[38;5;24mOOO)[0m
+[38;5;12mPXX/[38;5;24mOOOOOOOOOOOOO[38;5;255mOO[38;5;24mOOOj[0m
+[38;5;12mPX/[38;5;24mOOOOOOOOOOOOOOOOOO)[0m
+[38;5;12mL/[38;5;24mOOOOOOOOOOOOOOOOO)[0m
+[38;5;12m  [38;5;12m___ _ _             [38;5;24m_    _          [38;5;12m
+ / __(@) |_ _____ _ _[38;5;24m| |__| |_  _ ___ [38;5;12m
+ \__ \ | \ V / -_) '_[38;5;24m| '_ \ | || / -_)[38;5;12m
+ |___/_|_|\_/\___|_| [38;5;24m|_.__/_|\_,_\___|[0m
+X0
+exit 0
